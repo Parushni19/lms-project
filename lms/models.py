21ea3a029affd83ada23dbf878ac7b0b8e2c78e7
Parushni19 <parushnibunny@gmail.com>
2026-08-03 00:29:19 +0530

    Fixed Profile model issue

--- lms/models.py
+++ lms/models.py
@@ -94,30 +94,12 @@ class Profile(models.Model):
 
     def __str__(self):
         return self.user.username
-    
-
-
-class Profile(models.Model):
-    user = models.OneToOneField(
-        User,
-        on_delete=models.CASCADE
-    )
-
-    image = models.ImageField(
-        default='default.png',
-        upload_to='profile_pics'
-    )
-
-    def __str__(self):
-        return self.user.username
-
 
 @receiver(post_save, sender=User)
 def create_profile(sender, instance, created, **kwargs):
     if created:
         Profile.objects.create(user=instance)
 
-
 @receiver(post_save, sender=User)
 def save_profile(sender, instance, **kwargs):
-    instance.profile.save()
+    Profile.objects.get_or_create(user=instance)
